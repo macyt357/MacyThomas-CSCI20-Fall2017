4c3a181420980d32cb3d0ee48ddf142e236b574e
Macy Thomas <mthomas013@student.butte.edu>
2017-08-23 19:20:08 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,16 @@
+/*Game Set Up
+The game will be set up with a number range from 0 to 21
+A variable 'x' will be defined to randomly select a value between those numbers
+
+Game Play
+The player will make a guess based on what number they believe it is
+If the player guesses correctly then the game will end and the word "Congratulations!" will appear on the screen
+If the player guesses incorrectly they will lose one point from ten and have nine more turns to guess
+also when the player guesses incorrectly either the word "higher" will appear on screen indicating for the player to guess a number above their original guess, 
+or the word "lower" will appear indicating for them to guess a number below their original guess
+After ten turns if the player has not guessed correctly they will have lost all ten points, "You Lose" will appear on screen, and the game will reset with a new number
+*/
+
+git add lab11
+git commit -a -m 'Lab1.1 submission'
+git push
