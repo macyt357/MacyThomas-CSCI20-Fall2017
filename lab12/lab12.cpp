e8e09de4dd167d3f00cd4fd727001885a3bcfa86
Macy Thomas <mthomas013@student.butte.edu>
2017-08-28 18:54:06 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   cout << "  _     _  " << endl;
+   cout << " /  \\_/  \\" << endl;
+   cout << " \\       /" << endl;
+   cout << "  \\     /" << endl;
+   cout << "   \\   /" << endl;
+   cout << "    \\_/" << endl;
+   
+   return 0;
+}
