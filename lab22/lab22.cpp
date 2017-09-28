310174d3df4d15d27f6d62d3749db39b0a4b2857
Macy Thomas <mthomas013@student.butte.edu>
2017-09-28 17:15:41 +0000

    Lab 2.2 submission

---
+++ lab22/lab22.cpp
@@ -0,0 +1,20 @@
+//Macy Thomas
+//Lab 2.2
+//Random Number Generator
+
+#include <iostream>
+#include <time.h> //allows for us to use srand
+using namespace std;
+
+void Random(){
+    cout << "Your Random Number is: "<< endl; //outputs the phrase "Your Random Number is:"
+    srand (time(NULL)); //produces random number
+    int number = rand() % 100 + 1; //sets boundary between 0-100
+    cout << number; //outputs our random number
+}
+int main(){
+    Random(); //runs our function
+}
+
+
+
